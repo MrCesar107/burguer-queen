f0f9543393815163bb57869df30ffc1a00b525ae
MrCesar107 <cesar.rodriguez.lara54@gmail.com>
2019-12-31 15:02:54 -0600

    Redux actions and constants for menus

---
+++ src/actions/menu.actions.js
@@ -0,0 +1,24 @@
+import { menuConstants } from '../constants';
+import { waiterService } from '../services';
+import { alertActions } from './';
+import { history } from '../helpers';
+
+export const menuActions = {
+  createMenu,
+  updateMenu,
+  deleteMenu,
+  getAll,
+};
+
+function createMenu() {
+}
+
+function updateMenu() {
+}
+
+function deleteMenu() {
+}
+
+function getAll() {
+  
+}
